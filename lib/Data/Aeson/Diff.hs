9ab3557014cf78d4f68498bacd942e9edcef3944
Thomas Sutton <me@thomas-sutton.id.au>
2014-11-17 17:19:45 +1100

    Stubs of explode/collapse utility functions

--- lib/Data/Aeson/Diff.hs
+++ lib/Data/Aeson/Diff.hs
@@ -1,11 +1,19 @@
 module Data.Aeson.Diff (
+    Patch,
+    Path,
+    Key(..),
+    -- * Functions
     diff,
     patch,
-    Patch,
+    -- * Utility functions
+    explode,
+    collapse,
 ) where
 
 import Data.Aeson
+import qualified Data.HashMap.Strict as HM
 import Data.Text (Text)
+import qualified Data.Vector as V
 
 -- | A path through a JSON document is a possibly empty sequence of 'Key's.
 type Path = [Key]
@@ -38,4 +46,23 @@ patch _ _ = error "Data.Aeson.Diff.patch: not implemented"
 explode
     :: Value
     -> [(Path, Value)]
-explode document = error "Data.Aeson.Diff.explode: not implemented"
+explode = worker []
+  where
+    worker prefix doc = case doc of
+        Array arr  -> concatMap (\(n,v) -> worker (AKey n:prefix) v)
+            $ zip [0..] $ V.toList arr
+        Object obj -> concatMap (\(k,v) -> worker (OKey k:prefix) v) $ HM.toList obj
+        atomic     -> [(prefix, atomic)]
+
+-- | Collapse pairs of 'Path's and atomic values into a JSON document.
+collapse
+    :: [(Path, Value)]
+    -> Value
+collapse = foldl work Null
+  where
+    work doc ([],  val) = val
+    work (Array arr) (AKey k:rest, val) = Array arr
+    work (Object obj) (OKey k:rest, val) = Object $ modifyObj obj k (const)
+    work _ _ = error "Data.Aeson.Diff.collapse: not implemented"
+
+    modifyObj obj k f = HM.alter obj k f
--- test/properties.hs
+++ test/properties.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Test.QuickCheck
@@ -6,8 +7,16 @@ import Test.QuickCheck.All
 
 import Data.Aeson
 import Data.Aeson.Diff
+import qualified Data.Vector as V
 
-instance Arbitrary Value
+instance Arbitrary Value where
+    arbitrary = elements
+        [ Null
+        , Bool True
+        , Number 123
+        , String "boo"
+        , Array . V.fromList $ [String "123", Number 123]
+        ]
 
 -- | Check that extracting and applying a diff results in the targf
 prop_DiffApply
@@ -16,7 +25,13 @@ prop_DiffApply
     -> Bool
 prop_DiffApply f t = t == patch (diff f t) f
 
--- 
+-- | Check that (collapse . explode) == id
+prop_ExplodeCollapse
+    :: Value
+    -> Bool
+prop_ExplodeCollapse doc = doc == (collapse . explode) doc
+
+--
 -- Use Template Haskell to automatically run all of the properties above.
 --
 
